403de5561efd0bffae9e6617c487b0bc3aad8746
favorcat <favorcat.me@gmail.com>
2022-04-14 15:59:58 +0900

    MUSTYLINK vue project

---
+++ MUSTYLINK-vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <styleChecker/>
+</template>
+
+<script setup>
+import styleChecker from './components/styleChecker.vue'
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+body {
+  background-color: white;
+}
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Malgun Gothic', 'Open Sans', 'Helvetica Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  font-weight: bold;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
